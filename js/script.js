e99831aea24f379ad5cb9ba375416e989be220ae
Matthew Wallace <mjwallace99@hotmail.com>
2025-05-09 12:08:59 -0600

    Part 2 completed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,36 @@ function changeText() {
 
 }
 
-function add(num1, num2) {
-    console.log(num1 + num2);
+
+function priceChange(price1, price2){
+    let price;
+    if (price2>price1){
+        price=alert('this card is becoming popular!');
+    }
+    else if(price1>price2){
+        price=alert('this card has dropped in price!');
+    }
+    console.log(price);
+
+}
+priceChange(5,6)
+priceChange(6,5)
+
+function addCard(deck, card){
+    deck.push(card);
+    console.log(deck);
+    return deck;
 }
 
-add(5, 10);
+cDeck=['Atraxa', 'Genesis Wave', 'Smothering Tithe'];
+addCard(cDeck, 'Counter Spell')
+console.log(cDeck);
+
+function isCommander(deck, pos){
+    let commander=deck[pos];
+    console.log("Your Commander is " + commander + "!");
+
+}
+
+isCommander(cDeck, 0)
+
